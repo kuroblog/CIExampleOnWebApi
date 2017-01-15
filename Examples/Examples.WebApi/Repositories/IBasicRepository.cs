5c35053565fba40b6146ddb759ed1e6642415e1c
Kuro <kuro_q@live.com>
2017-01-15 19:29:47 +0800

    -> add IBasicRepository

---
+++ Examples/Examples.WebApi/Repositories/IBasicRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Examples.WebApi.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class IBasicRepository
+    {
+    }
+}
