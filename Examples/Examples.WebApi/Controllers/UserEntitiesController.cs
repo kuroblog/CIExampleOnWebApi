2c294830d3eca1125d43618500ba3ba23fae4dde
Kuro <kuro_q@live.com>
2017-01-27 14:07:55 +0800

    -> add publish-config-file

--- Examples/Examples.WebApi/Controllers/UserEntitiesController.cs
+++ Examples/Examples.WebApi/Controllers/UserEntitiesController.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Examples.WebApi.Controllers
 {
-    using Examples.WebApi.Models;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
