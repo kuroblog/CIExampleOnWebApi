22b2a693796bbae946ed8235aa1ea7346d9bd7b2
Kuro <kuro_q@live.com>
2017-02-05 19:30:23 +0800

    -> ver.2 completed

---
+++ Examples/Examples.Ci.WebApi.Tests/Controllers/UsersController_Tests.cs
@@ -0,0 +1,428 @@
+﻿
+namespace Examples.Ci.WebApi.Controllers.Tests
+{
+    using Controllers;
+    using Ef.Entities;
+    using Ef.Repositories;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models;
+    using Moq;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using System.Net;
+    using System.Web.Http.Results;
+
+    [TestClass]
+    public class UsersController_Tests
+    {
+        #region Initialize
+        [TestInitialize]
+        public void Initialize()
+        {
+            testItems = new List<UserEntity> {
+                new UserEntity { CreatedAt = DateTime.Now, UserNo = "001", UserName = "test1" },
+                new UserEntity { CreatedAt = DateTime.Now, UserNo = "002", UserName = "test2" },
+                new UserEntity { CreatedAt = DateTime.Now, UserNo = "003", UserName = "test3" }
+            };
+
+            mockUserRepo = new Mock<IUserRepository>();
+            mockUserRepo.Setup(m => m.View).Returns(() => testItems.AsQueryable());
+
+            controller = new UsersController(mockUserRepo.Object);
+        }
+
+        private UsersController controller;
+        private List<UserEntity> testItems;
+        private Mock<IUserRepository> mockUserRepo;
+        #endregion
+
+        #region GetUsers
+        [TestMethod]
+        public void GetUsers_Test_Successed()
+        {
+            var actual = controller.GetUsers().Result;
+            Assert.IsNotNull(actual);
+
+            var actualResult = actual as OkNegotiatedContentResult<IQueryable<UserDto>>;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(testItems.Count, actualResult.Content.Count());
+        }
+
+        [TestMethod]
+        public void GetUsers_Test_Not_Found()
+        {
+            mockUserRepo.Setup(m => m.View).Returns(() => new List<UserEntity>().AsQueryable());
+
+            var actual = controller.GetUsers().Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void GetUsers_Test_Exception()
+        {
+            mockUserRepo.Setup(m => m.View).Callback(() =>
+            {
+                throw new Exception("mock test");
+            });
+
+            var actual = controller.GetUsers().Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ExceptionResult));
+        }
+        #endregion
+
+        #region GetUsersByName
+        [TestMethod]
+        public void GetUsersByName_Test_Successed_When_Single_Matched()
+        {
+            var expected = testItems[1];
+
+            var actual = controller.GetUsersByName(expected.UserName).Result;
+            Assert.IsNotNull(actual);
+
+            var actualResult = actual as OkNegotiatedContentResult<IQueryable<UserDto>>;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(1, actualResult.Content.Count());
+            Assert.AreEqual(expected.UserName, actualResult.Content.FirstOrDefault().UserName);
+        }
+
+        [TestMethod]
+        public void GetUsersByName_Test_Successed_When_Multiple_Matched()
+        {
+            var expected = "test";
+
+            var actual = controller.GetUsersByName(expected).Result;
+            Assert.IsNotNull(actual);
+
+            var actualResult = actual as OkNegotiatedContentResult<IQueryable<UserDto>>;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(testItems.Count, actualResult.Content.Count());
+        }
+
+        [TestMethod]
+        public void GetUsersByName_Test_Successed_When_Not_Found()
+        {
+            var expected = "999";
+
+            var actual = controller.GetUsersByName(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void GetUsersByName_Test_Bad_Request_With_Message_When_Args_Is_Empty()
+        {
+            var expected = string.Empty;
+
+            var actual = controller.GetUsersByName(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(BadRequestErrorMessageResult));
+        }
+        #endregion
+
+        #region GetUserByNo
+        [TestMethod]
+        public void GetUserByNo_Test_Successed()
+        {
+            var expected = testItems[1];
+
+            var actual = controller.GetUserByNo(expected.UserNo).Result;
+            Assert.IsNotNull(actual);
+
+            var actualResult = actual as OkNegotiatedContentResult<UserDto>;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(expected.UserNo, actualResult.Content.UserNo);
+        }
+
+        [TestMethod]
+        public void GetUserByNo_Test_Not_Found()
+        {
+            var expected = "999";
+
+            var actual = controller.GetUserByNo(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void GetUserByNo_Test_Bad_Request_With_Message_When_Args_Is_Empty()
+        {
+            var expected = string.Empty;
+
+            var actual = controller.GetUserByNo(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(BadRequestErrorMessageResult));
+        }
+        #endregion
+
+        #region PostUser
+        [TestMethod]
+        public void PostUser_Test_Successed()
+        {
+            var expected = new UserDto
+            {
+                UserNo = "999",
+                UserName = "test-user"
+            };
+
+            mockUserRepo.Setup(m => m.Create()).Returns(() => new UserEntity { });
+            mockUserRepo.Setup(m => m.Insert(It.IsAny<UserEntity>())).Returns(() => 1);
+
+            var actual = controller.PostUser(expected).Result;
+            Assert.IsNotNull(actual);
+
+            var actualResult = actual as CreatedAtRouteNegotiatedContentResult<int>;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(1, actualResult.Content);
+            Assert.AreEqual(expected.UserNo, actualResult.RouteValues[nameof(expected.UserNo)]);
+        }
+
+        [TestMethod]
+        public void PostUser_Test_Bad_Request_With_Message_When_Args_Is_Null()
+        {
+            UserDto expected = null;
+
+            var actual = controller.PostUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(BadRequestErrorMessageResult));
+        }
+
+        [TestMethod]
+        public void PostUser_Test_Exception()
+        {
+            var expected = new UserDto { };
+
+            mockUserRepo.Setup(m => m.Create()).Returns(() => new UserEntity { });
+            mockUserRepo.Setup(m => m.Insert(It.IsAny<UserEntity>())).Callback(() =>
+            {
+                throw new Exception("mock test");
+            });
+
+            var actual = controller.PostUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ExceptionResult));
+        }
+
+        [TestMethod]
+        public void PostUser_Test_Db_Update_Exception()
+        {
+            var expected = new UserDto { };
+
+            mockUserRepo.Setup(m => m.Create()).Returns(() => new UserEntity { });
+            mockUserRepo.Setup(m => m.Insert(It.IsAny<UserEntity>())).Callback(() =>
+            {
+                throw new DbUpdateException("mock test");
+            });
+
+            var actual = controller.PostUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ExceptionResult));
+        }
+
+        [TestMethod]
+        public void PostUser_Test_Db_Update_Exception_When_Conflict()
+        {
+            var expected = new UserDto
+            {
+                UserNo = testItems[1].UserNo,
+                UserName = testItems[1].UserName
+            };
+
+            mockUserRepo.Setup(m => m.Create()).Returns(() => new UserEntity { });
+            mockUserRepo.Setup(m => m.Insert(It.IsAny<UserEntity>())).Callback(() =>
+            {
+                throw new DbUpdateException("mock test");
+            });
+
+            var actual = controller.PostUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ConflictResult));
+        }
+        #endregion
+
+        #region DeleteUser
+        [TestMethod]
+        public void DeleteUser_Test_Successed()
+        {
+            var expected = testItems[1];
+
+            mockUserRepo.Setup(m => m.Delete(It.IsAny<UserEntity>())).Returns(() => 1);
+
+            var actual = controller.DeleteUser(expected.UserNo).Result;
+            Assert.IsNotNull(actual);
+
+            var actualResult = actual as OkNegotiatedContentResult<string>;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(expected.UserNo, actualResult.Content);
+        }
+
+        [TestMethod]
+        public void DeleteUser_Test_Bad_Request_With_Message_When_Args_Is_Null()
+        {
+            var actual = controller.DeleteUser(string.Empty).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(BadRequestErrorMessageResult));
+        }
+
+        [TestMethod]
+        public void DeleteUserByNo_Test_Not_Found()
+        {
+            var expected = "999";
+
+            var actual = controller.DeleteUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void DeleteUser_Test_Exception()
+        {
+            var expected = testItems[1];
+
+            mockUserRepo.Setup(m => m.Delete(It.IsAny<UserEntity>())).Callback(() =>
+            {
+                throw new Exception("mock test");
+            });
+
+            var actual = controller.DeleteUser(expected.UserNo).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ExceptionResult));
+        }
+
+        [TestMethod]
+        public void DeleteUser_Test_Db_Update_Concurrency_Exception()
+        {
+            var expected = testItems[1];
+
+            mockUserRepo.Setup(m => m.Delete(It.IsAny<UserEntity>())).Callback(() =>
+            {
+                throw new DbUpdateConcurrencyException("mock test");
+            });
+
+            var actual = controller.DeleteUser(expected.UserNo).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ExceptionResult));
+        }
+
+        [TestMethod]
+        public void DeleteUser_Test_Db_Update_Exception_When_No_Content()
+        {
+            var expected = testItems[1];
+
+            mockUserRepo.Setup(m => m.Delete(It.IsAny<UserEntity>())).Callback((UserEntity p) =>
+            {
+                testItems.Remove(p);
+                throw new DbUpdateConcurrencyException("mock test");
+            });
+
+            var actual = controller.DeleteUser(expected.UserNo).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(StatusCodeResult));
+
+            var actualResult = actual as StatusCodeResult;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(HttpStatusCode.NoContent, actualResult.StatusCode);
+        }
+        #endregion
+
+        #region PutUser
+        [TestMethod]
+        public void PutUser_Test_Successed()
+        {
+            var expected = new UserDto
+            {
+                UserNo = testItems[1].UserNo,
+                UserName = testItems[1].UserName
+            };
+
+            mockUserRepo.Setup(m => m.Update(It.IsAny<UserEntity>())).Returns(() => 1);
+
+            var actual = controller.PutUser(expected).Result;
+            Assert.IsNotNull(actual);
+
+            var actualResult = actual as StatusCodeResult;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(HttpStatusCode.NoContent, actualResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void PutUser_Test_Bad_Request_With_Message_When_Args_Is_Null()
+        {
+            var actual = controller.PutUser(null).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(BadRequestErrorMessageResult));
+        }
+
+        [TestMethod]
+        public void PutUserByNo_Test_Not_Found()
+        {
+            var expected = new UserDto { UserNo = "999" };
+
+            var actual = controller.PutUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void PutUser_Test_Exception()
+        {
+            var expected = new UserDto
+            {
+                UserNo = testItems[1].UserNo,
+                UserName = testItems[1].UserName
+            };
+
+            mockUserRepo.Setup(m => m.Update(It.IsAny<UserEntity>())).Callback(() =>
+            {
+                throw new Exception("mock test");
+            });
+
+            var actual = controller.PutUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ExceptionResult));
+        }
+
+        [TestMethod]
+        public void PutUser_Test_Db_Update_Concurrency_Exception()
+        {
+            var expected = new UserDto
+            {
+                UserNo = testItems[1].UserNo,
+                UserName = testItems[1].UserName
+            };
+
+            mockUserRepo.Setup(m => m.Update(It.IsAny<UserEntity>())).Callback(() =>
+            {
+                throw new DbUpdateConcurrencyException("mock test");
+            });
+
+            var actual = controller.PutUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(ExceptionResult));
+        }
+
+        [TestMethod]
+        public void PutUser_Test_Db_Update_Exception_When_Not_Found()
+        {
+            var expected = new UserDto
+            {
+                UserNo = testItems[1].UserNo,
+                UserName = testItems[1].UserName
+            };
+
+            mockUserRepo.Setup(m => m.Update(It.IsAny<UserEntity>())).Callback((UserEntity p) =>
+            {
+                testItems.Remove(p);
+                throw new DbUpdateConcurrencyException("mock test");
+            });
+
+            var actual = controller.PutUser(expected).Result;
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(NotFoundResult));
+        }
+        #endregion
+    }
+}
--- Examples/Examples.Ci.WebApi/Controllers/UsersController.cs
+++ Examples/Examples.Ci.WebApi/Controllers/UsersController.cs
@@ -4,7 +4,10 @@ namespace Examples.Ci.WebApi.Controllers
     using Core.Utilities;
     using Ef.Repositories;
     using Models;
+    using System;
+    using System.Data.Entity.Infrastructure;
     using System.Linq;
+    using System.Net;
     using System.Threading.Tasks;
     using System.Web.Http;
 
@@ -34,15 +37,7 @@ namespace Examples.Ci.WebApi.Controllers
         {
             var result = await Runner.Execute(() =>
             {
-                //return userRepo.View;
-
                 return userRepo.View.Select(UserDto.UserEntityParse);
-
-                // 使用注入的 DbContext 来进行操作
-                //return context.Set<UserEntity>().Select(UserDto.UserEntityParse);
-
-                // 非注入，直接使用 DbContext 来进行操作
-                //return db.Users.Select(UserDto.UserEntityParse);
             });
 
             if (result.HasError)
@@ -57,6 +52,177 @@ namespace Examples.Ci.WebApi.Controllers
             {
                 return Ok(result.Content);
             }
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetUsersByName(string userName)
+        {
+            if (string.IsNullOrEmpty(userName))
+            {
+                return BadRequest(nameof(userName));
+            }
+
+            var result = await Runner.Execute(() =>
+            {
+                return userRepo.View.Select(UserDto.UserEntityParse).Where(p => p.UserName.Contains(userName));
+            });
+
+            if (result.HasError)
+            {
+                return InternalServerError(result.Error);
+            }
+            else if (result.Content == null || result.Content.Count() <= 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(result.Content);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetUserByNo(string userNo)
+        {
+            if (string.IsNullOrEmpty(userNo))
+            {
+                return BadRequest(nameof(userNo));
+            }
+
+            var result = await Runner.Execute(() =>
+            {
+                return userRepo.View.Select(UserDto.UserEntityParse).FirstOrDefault(p => p.UserNo == userNo);
+            });
+
+            if (result.HasError)
+            {
+                return InternalServerError(result.Error);
+            }
+            else if (result.Content == null || string.IsNullOrEmpty(result.Content.UserNo))
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(result.Content);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> PostUser(UserDto userDto)
+        {
+            if (userDto == null)
+            {
+                return BadRequest(nameof(userDto));
+            }
+
+            var result = await Runner.Execute(() =>
+            {
+                var user = userRepo.Create();
+                user.UserNo = userDto.UserNo;
+                user.UserName = userDto.UserName;
+                user.CreatedAt = DateTime.Now;
+
+                return userRepo.Insert(user);
+            });
+
+            if (result.HasError)
+            {
+                if (result.Error is DbUpdateException)
+                {
+                    var isExists = userRepo.View.Count(p => p.UserNo == userDto.UserNo) > 0;
+                    if (isExists)
+                    {
+                        return Conflict();
+                    }
+                }
+
+                return InternalServerError(result.Error);
+            }
+
+            return CreatedAtRoute("DefaultApi", new { userDto.UserNo }, result.Content);
+        }
+
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteUser(string userNo)
+        {
+            if (string.IsNullOrEmpty(userNo))
+            {
+                return BadRequest(nameof(userNo));
+            }
+
+            var result = await Runner.Execute(() =>
+            {
+                var user = userRepo.View.FirstOrDefault(p => p.UserNo == userNo);
+                if (user == null)
+                {
+                    return 0;
+                }
+
+                return userRepo.Delete(user);
+            });
+
+            if (result.HasError)
+            {
+                if (result.Error is DbUpdateConcurrencyException)
+                {
+                    var isDeleted = userRepo.View.Count(p => p.UserNo == userNo) == 0;
+                    if (isDeleted)
+                    {
+                        return StatusCode(HttpStatusCode.NoContent);
+                    }
+                }
+
+                return InternalServerError(result.Error);
+            }
+            else if (result.Content == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(userNo);
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> PutUser(UserDto userDto)
+        {
+            if (userDto == null)
+            {
+                return BadRequest(nameof(userDto));
+            }
+
+            var result = await Runner.Execute(() =>
+            {
+                var user = userRepo.View.FirstOrDefault(p => p.UserNo == userDto.UserNo);
+                if (user == null)
+                {
+                    return 0;
+                }
+
+                user.UserName = userDto.UserName;
+
+                return userRepo.Update(user);
+            });
+
+            if (result.HasError)
+            {
+                if (result.Error is DbUpdateConcurrencyException)
+                {
+                    var isDeleted = userRepo.View.Count(p => p.UserNo == userDto.UserNo) == 0;
+                    if (isDeleted)
+                    {
+                        return NotFound();
+                    }
+                }
+
+                return InternalServerError(result.Error);
+            }
+            else if (result.Content == 0)
+            {
+                return NotFound();
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
         }
     }
 }
