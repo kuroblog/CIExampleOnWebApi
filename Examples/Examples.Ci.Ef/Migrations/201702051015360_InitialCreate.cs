7a1018712da6f8d495e4670829d33a9dc1ac481c
Kuro <kuro_q@live.com>
2017-02-05 20:37:38 +0800

    -> add migration

---
+++ Examples/Examples.Ci.Ef/Migrations/201702051015360_InitialCreate.cs
@@ -0,0 +1,31 @@
+namespace Examples.Ci.Ef.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.t_users",
+                c => new
+                    {
+                        id = c.Guid(nullable: false, identity: true),
+                        created_at = c.DateTime(nullable: false),
+                        updated_at = c.DateTime(nullable: false),
+                        user_no = c.String(nullable: false, maxLength: 30),
+                        user_name = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.id)
+                .Index(t => t.user_no, unique: true);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.t_users", new[] { "user_no" });
+            DropTable("dbo.t_users");
+        }
+    }
+}
---
+++ Examples/Examples.Ci.Ef/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Examples.Ci.Ef.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Examples.Ci.Ef.CiContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Examples.Ci.Ef.CiContext";
+        }
+
+        protected override void Seed(Examples.Ci.Ef.CiContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
