3b9779915c68fc99148e74d9e4b943b52dc11111
Kuro <kuro_q@live.com>
2017-03-04 20:11:20 +0800

    -> add microsoft.owin references

---
+++ Examples/Examples.Ci.WebApi.Tests/Controllers/Func/DefaultController_Func_Tests.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Examples.Ci.WebApi.Tests.Controllers.Func
+{
+    using Microsoft.Owin.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DefaultController_Func_Tests
+    {
+        [TestMethod]
+        public void DefaultController_Test_Successed()
+        {
+            using (var server = TestServer.Create(app => { }))
+            {
+                var result = server.HttpClient.GetAsync("api/default").Result;
+            }
+
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
